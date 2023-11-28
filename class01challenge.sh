e6445d0aec3dcffad4f80a09f027db8319df2a52
Will <wrbaur@gmail.com>
2023-11-28 16:09:23 -0600

    class01challenge.sh

--- class01challenge.sh
+++ class01challenge.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Script: Class 01 challenge syslog_backup.sh
+# Date 11/28/2023 
+# Will Baur 
 # Description:
 # Copies /var/log/syslog to the current working directory, appends the current date and time to the filename,
 #              and includes timestamped echo statements.
@@ -10,20 +12,20 @@ timestamp() {
   echo "[$(date '+%Y-%m-%d')]"
 }
 
-# Echo statement with timestamp
+# Echo statement with timestamp letting user know process has begun
 echo "$(timestamp) Starting syslog backup..."
 
-# Source and destination file paths
+# The File Source and destination file path where we are storing the file
 source_file="/var/log/syslog"
 destination_file="syslog_backup_$(date '+%Y%m%d').log"
 
 # Echo statement with timestamp
 echo "$(timestamp) Copying syslog to $destination_file..."
 
-# Copy syslog to the current working directory
+# Copy syslog to the current directory
 cp "$source_file" "$destination_file"
 
-# Check if the copy was successful
+# Perform a check to see if the copy was successful
 if [ $? -eq 0 ]; then
   echo "$(timestamp) Copy successful!"
 else
@@ -31,7 +33,7 @@ else
   exit 1
 fi
 
-# Echo statement with timestamp
+# Let the user know where the new file is stored and its name
 echo "$(timestamp) Backup complete. The file is stored as: $destination_file"
 
 # This scrips was created with help from ChatGTP3.5
